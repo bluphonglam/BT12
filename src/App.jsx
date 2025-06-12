1637312f172b7c76877ec6fa366a6296276fd2e3
lam <bluplam5@gmail.com>
2025-06-12 20:27:52 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import './App.css';
+import Cat from './svg/cat.svg';
+import Dog from './svg/dog.svg';
+import Bird from './svg/bird.svg';
+import Cow from './svg/cow.svg';
+import Gator from './svg/gator.svg';
+import Horse from './svg/horse.svg';
+import Heart from './svg/heart.svg';
+import AnimalsShow from './components/AnimalsShow';
+
+function App() {
+  const [animals, setAnimals] = useState([]);
+  const animalSvgs = [Cat, Dog, Bird, Cow, Gator, Horse];
+
+  const handleAddAnimal = () => {
+    const randomAnimal = animalSvgs[Math.floor(Math.random() * animalSvgs.length)];
+    setAnimals([...animals, { url: randomAnimal, heartSize: 20 }]);
+  };
+
+  const handleHeartClick = (index) => {
+    const updated = animals.map((animal, i) =>
+      i === index ? { ...animal, heartSize: animal.heartSize + 9 } : animal
+    );
+    setAnimals(updated);
+  };
+
+  return (
+    <div className="app-container">
+      <h1 className="app-title">Add Animal </h1>
+
+      <div className="button-wrapper">
+        <button onClick={handleAddAnimal} className="animal-button">
+          Add yes
+        </button>
+      </div>
+
+      <div className="animal-gallery">
+        {animals.map((animal, index) => (
+          <AnimalsShow
+            key={index}
+            animal={animal}
+            heartIcon={Heart}
+            onHeartClick={() => handleHeartClick(index)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default App;
